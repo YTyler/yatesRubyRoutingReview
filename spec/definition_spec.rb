104d09f6823a92699ddf00290c73e12a3fed16e1
Tyler C. Yates <tyates907@gmail.com>
2020-01-05 19:16:04 -0800

    finish "#==" test

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -5,8 +5,21 @@ require 'definition'
 
 describe('.#Definition') do
 
+  before(:each) do
+    @word = Word.new("Test", nil)
+    @word.keep
+  end
+
+  after(:each) do
+    Word.clear
+  end
+
   describe('#==') do
-    it('two definitions are equal if their text strings match exactly')
+    it('two definitions are equal if their text strings match exactly') do
+      definition1 = Definition.new("Example Definition", @word.id, nil)
+      definition2 = Definition.new("Example Definition", @word.id, nil)
+      expect(definition1).to(eq(definition2))
+    end
   end
 
 end
