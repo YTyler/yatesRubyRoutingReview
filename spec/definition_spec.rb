c067c3e6c100d64a5c66a60c037a807bd3eae42b
Tyler C. Yates <tyates907@gmail.com>
2020-01-05 18:18:21 -0800

    create "#==" test for Definition class

---
+++ spec/definition_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require 'pry'
+require 'word'
+require 'definition'
+
+describe('.#Definition') do
+
+  describe('#==') do
+    it('two definitions are equal if their text strings match exactly')
+  end
+
+end
